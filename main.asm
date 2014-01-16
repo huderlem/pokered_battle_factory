f36dbc061979dc9a72488e23eb5759604334e00a
Marcus Huderle <huderlem@gmail.com>
2014-01-16 12:02:09 -0600

    All trainers use generic AI.

--- main.asm
+++ main.asm
@@ -58390,7 +58390,7 @@ TrainerAIPointers: ; 3a55c (e:655c)
 	dbw 3,GenericAI
 	dbw 3,GenericAI
 	dbw 3,GenericAI
-	dbw 3,JugglerAI ; juggler_x
+	dbw 3,GenericAI ; juggler_x
 	dbw 3,GenericAI
 	dbw 3,GenericAI
 	dbw 3,GenericAI
@@ -58401,30 +58401,30 @@ TrainerAIPointers: ; 3a55c (e:655c)
 	dbw 3,JugglerAI ; juggler
 	dbw 3,GenericAI
 	dbw 3,GenericAI
-	dbw 2,BlackbeltAI ; blackbelt
+	dbw 3,GenericAI ; blackbelt
 	dbw 3,GenericAI
 	dbw 3,GenericAI
 	dbw 1,GenericAI ; chief
 	dbw 3,GenericAI
-	dbw 1,GiovanniAI ; giovanni
+	dbw 3,GenericAI ; giovanni
 	dbw 3,GenericAI
-	dbw 2,CooltrainerMAI ; cooltrainerm
-	dbw 1,CooltrainerFAI ; cooltrainerf
-	dbw 2,BrunoAI ; bruno
-	dbw 5,BrockAI ; brock
-	dbw 1,MistyAI ; misty
-	dbw 1,LtSurgeAI ; surge
-	dbw 1,ErikaAI ; erika
-	dbw 2,KogaAI ; koga
-	dbw 2,BlaineAI ; blaine
-	dbw 1,SabrinaAI ; sabrina
+	dbw 3,GenericAI ; cooltrainerm
+	dbw 3,GenericAI ; cooltrainerf
+	dbw 3,GenericAI ; bruno
+	dbw 3,GenericAI ; brock
+	dbw 3,GenericAI ; misty
+	dbw 3,GenericAI ; surge
+	dbw 3,GenericAI ; erika
+	dbw 3,GenericAI ; koga
+	dbw 3,GenericAI ; blaine
+	dbw 3,GenericAI ; sabrina
 	dbw 3,GenericAI
-	dbw 1,Sony2AI ; sony2
-	dbw 1,Sony3AI ; sony3
-	dbw 2,LoreleiAI ; lorelei
+	dbw 3,GenericAI ; sony2
+	dbw 3,GenericAI ; sony3
+	dbw 3,GenericAI ; lorelei
 	dbw 3,GenericAI
-	dbw 2,AgathaAI ; agatha
-	dbw 1,LanceAI ; lance
+	dbw 3,GenericAI ; agatha
+	dbw 3,GenericAI ; lance
 
 JugglerAI: ; 3a5e9 (e:65e9)
 	cp $40
