0b4727a007b4ed399c64410529551112b5142725
Marcus Huderle <huderlem@gmail.com>
2014-01-20 11:05:20 -0600

    Changed AI for all trainers to the same as Lance.

--- main.asm
+++ main.asm
@@ -57136,40 +57136,40 @@ ReadMove: ; 39884 (e:5884)
 ; move choice modification methods that are applied for each trainer class
 ; 0 is sentinel value
 TrainerClassMoveChoiceModifications: ; 3989b (e:589b)
-	db 0      ; YOUNGSTER
-	db 1,0    ; BUG CATCHER
-	db 1,0    ; LASS
+	db 1,3,0      ; YOUNGSTER
+	db 1,3,0    ; BUG CATCHER
+	db 1,3,0    ; LASS
 	db 1,3,0  ; SAILOR
-	db 1,0    ; JR__TRAINER_M
-	db 1,0    ; JR__TRAINER_F
-	db 1,2,3,0; POKEMANIAC
-	db 1,2,0  ; SUPER_NERD
-	db 1,0    ; HIKER
-	db 1,0    ; BIKER
+	db 1,3,0    ; JR__TRAINER_M
+	db 1,3,0    ; JR__TRAINER_F
+	db 1,3,0; POKEMANIAC
+	db 1,3,0  ; SUPER_NERD
+	db 1,3,0    ; HIKER
+	db 1,3,0    ; BIKER
 	db 1,3,0  ; BURGLAR
-	db 1,0    ; ENGINEER
-	db 1,2,0  ; JUGGLER_X
+	db 1,3,0    ; ENGINEER
+	db 1,3,0  ; JUGGLER_X
 	db 1,3,0  ; FISHER
 	db 1,3,0  ; SWIMMER
-	db 0      ; CUE_BALL
-	db 1,0    ; GAMBLER
+	db 1,3,0     ; CUE_BALL
+	db 1,3,0    ; GAMBLER
 	db 1,3,0  ; BEAUTY
-	db 1,2,0  ; PSYCHIC_TR
+	db 1,3,0  ; PSYCHIC_TR
 	db 1,3,0  ; ROCKER
-	db 1,0    ; JUGGLER
-	db 1,0    ; TAMER
-	db 1,0    ; BIRD_KEEPER
-	db 1,0    ; BLACKBELT
-	db 1,0    ; SONY1
+	db 1,3,0    ; JUGGLER
+	db 1,3,0    ; TAMER
+	db 1,3,0    ; BIRD_KEEPER
+	db 1,3,0    ; BLACKBELT
+	db 1,3,0    ; SONY1
 	db 1,3,0  ; PROF_OAK
-	db 1,2,0  ; CHIEF
-	db 1,2,0  ; SCIENTIST
+	db 1,3,0  ; CHIEF
+	db 1,3,0  ; SCIENTIST
 	db 1,3,0  ; GIOVANNI
-	db 1,0    ; ROCKET
+	db 1,3,0    ; ROCKET
 	db 1,3,0  ; COOLTRAINER_M
 	db 1,3,0  ; COOLTRAINER_F
-	db 1,0    ; BRUNO
-	db 1,0    ; BROCK
+	db 1,3,0    ; BRUNO
+	db 1,3,0    ; BROCK
 	db 1,3,0  ; MISTY
 	db 1,3,0  ; LT__SURGE
 	db 1,3,0  ; ERIKA
@@ -57179,9 +57179,9 @@ TrainerClassMoveChoiceModifications: ; 3989b (e:589b)
 	db 1,2,0  ; GENTLEMAN
 	db 1,3,0  ; SONY2
 	db 1,3,0  ; SONY3
-	db 1,2,3,0; LORELEI
-	db 1,0    ; CHANNELER
-	db 1,0    ; AGATHA
+	db 1,3,0; LORELEI
+	db 1,3,0    ; CHANNELER
+	db 1,3,0    ; AGATHA
 	db 1,3,0  ; LANCE
 
 TrainerPicAndMoneyPointers: ; 39914 (e:5914)
